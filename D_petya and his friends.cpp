1f01428665da42802cb5a329295bdb2fe43e0b4e
Osman-Goni22 <133319697+Osman-Goni22@users.noreply.github.com>
2024-02-15 13:29:22 +0600

    Create D_petya and his friends.cpp

---
+++ D_petya and his friends.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    if(n==2)
+    cout<<-1<<endl;
+    else
+    {
+    cout<<15<<endl;
+    cout<<10<<endl;
+    int x=n-2;
+    for(int i=1;i<n-1;i++)
+    {
+       cout<<6*i<<endl; 
+    }
+    }
+}
